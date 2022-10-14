fd14c815bb11f07a719549bc6b7f985a14a75e35
Slav Pilus <slav.pilus@kps.com>
2022-10-14 20:40:06 +0100

    rolling back to prod state

--- migrations/3_Slot.js
+++ migrations/3_Slot.js
@@ -7,5 +7,5 @@ module.exports = function (deployer, network, accounts) {
         '0x588acF052631756422844856e1dc2Ef6066ce121',
         '0x912aAEA32355DA6FeB20D98E73B9C81B5afd6A2e',
         new BN('10000000000000000000')
-        );
+    );
 };
