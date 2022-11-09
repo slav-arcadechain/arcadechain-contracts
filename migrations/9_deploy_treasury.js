96020e0b968b2b931facf5cc2379b7c76149b76a
Slav Pilus <slav.pilus@kps.com>
2022-11-09 12:40:41 +0000

    treasury fixed

--- migrations/9_deploy_treasury.js
+++ migrations/9_deploy_treasury.js
@@ -1,6 +1,6 @@
-const token = artifacts.require("TUSD");
-const act = artifacts.require("Act");
+const stableCoin = '0x912aAEA32355DA6FeB20D98E73B9C81B5afd6A2e';
 const Treasury = artifacts.require("Treasury");
+const act = '0x9AE51260C3824ADc9DD9F02Bc4D6B9e5Eddeb406';
 const percentageOfBurn = 5;
 const percentageOfGoldenTicket = 10;
 
@@ -12,8 +12,8 @@ module.exports = async function (deployer) {
     Treasury,
     admin,
     operator,
-    token.address,
-    act.address,
+    stableCoin,
+    act,
     percentageOfBurn,
     percentageOfGoldenTicket
   );
