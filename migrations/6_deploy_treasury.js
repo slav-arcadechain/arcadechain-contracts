14176e502993de0382e5054fecf3a8aacdd185bf
Slav Pilus <slav.pilus@kps.com>
2022-11-10 21:59:18 +0000

    treasury functional

--- migrations/6_deploy_treasury.js
+++ migrations/6_deploy_treasury.js
@@ -1,17 +1,18 @@
-const token = artifacts.require("TUSD");
 const Treasury = artifacts.require("Treasury");
-const percentageOfBurn = 10;
-const percentageOfGoldenTicket = 20;
+const stableCoin = '0x912aAEA32355DA6FeB20D98E73B9C81B5afd6A2e';
+const act = '0x9AE51260C3824ADc9DD9F02Bc4D6B9e5Eddeb406';
+const percentageOfBurn = 5;
+const percentageOfGoldenTicket = 10;
 
-module.exports = async function (deployer) {
-  let accounts = await web3.eth.getAccounts();
-  const admin = accounts[1];
-  const operator = accounts[2];
+module.exports = function (deployer) {
+  const admin = '0x588acF052631756422844856e1dc2Ef6066ce121';
+  const operator = '0x588acF052631756422844856e1dc2Ef6066ce121';
   deployer.deploy(
     Treasury,
     admin,
     operator,
-    token.address,
+    stableCoin,
+    act,
     percentageOfBurn,
     percentageOfGoldenTicket
   );
